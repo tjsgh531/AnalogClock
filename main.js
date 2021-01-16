54aa21b42dceb98cd5b4e562242fe35650e965a3
Guest <guest-fy0ri8@h26817.sgb.skir.kr>
2021-01-16 16:32:56 +0900

    storage

--- main.js
+++ main.js
@@ -2,14 +2,33 @@ const clockNum = document.getElementById('clock_num');
 positionClockNum();
 function positionClockNum(){
     clockNumArr = clockNum.children;
-    console.log(clockNumArr);
     for(let i = 0 ; i < clockNumArr.length ; i++){
-        console.log('실행?');
-        const deg = ((360 /12) * i)/180 * Math.PI;
-        const degx = Math.cos(deg) * 270;
-        const degy = Math.sin(deg) * 270;
-        console.log(degx);
-        clockNumArr.item(i).style.left =`calc(50%  -12px + ${degx})`;
-        clockNumArr.item(i).style.bottom =`calc(50% - 12px + ${degy});`;
+        const deg = ((360 /12) * (i -3))/180 * Math.PI;
+        const degx = Math.cos(deg) * 245;
+        const degy = Math.sin(deg) * 245;
+        
+        clockNumArr.item(i).style.transform = `translate(${degx}px,${degy}px)`;
+
+        if((i-3) % 3 === 0){
+            clockNumArr.item(i).style.fontSize = '42px';
+            clockNumArr.item(i).style.fontWeight ='900';
+        }
     }
 }
+
+function moveHand(){
+    const hour = document.getElementById('hour-hand');
+    const min = document.getElementById('min-hand');
+    const sec = document.getElementById('sec-hand');
+
+    const date = new Date();
+    const Hdeg = (360/12) * date.getHours() + (360 /12 /60) * date.getMinutes();
+    const Mdeg = (360 /60) * date.getMinutes();
+    const Sdeg = (360 / 60) * date.getSeconds();
+
+    hour.style.transform = `rotate(${Hdeg}turn)`;
+    min.style.transform = `rotate(${Mdeg}turn)`;
+    sec.style.transform = `rotete(${Sdeg}turn)`;
+}
+
+setInterval(moveHand,5000);
